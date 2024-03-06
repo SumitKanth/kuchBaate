a0e4ff19d1697870a31d82e9ba8ac650013c489d
sumit <sumitkanth999@gmail.com>
2024-03-06 15:43:03 +0530

    done changes for cors

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,16 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server:{
+    proxy:{
+      '/socket.io/': {
+        target: 'https://kuchbaatebackend.onrender.com',
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+      },
+
+    }
+},
   plugins: [react()],
 })
